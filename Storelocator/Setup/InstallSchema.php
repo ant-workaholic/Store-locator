b16ec9b205b1e921e80ec3e49960cc39d1ae410f
igor_che <igorchernin@yahoo.com>
2015-11-20 02:28:55 +0200

    Fixed issues in installSchema class.

--- Storelocator/Setup/InstallSchema.php
+++ Storelocator/Setup/InstallSchema.php
@@ -41,10 +41,47 @@ class InstallSchema implements InstallSchemaInterface
                  ->addColumn('name', Table::TYPE_TEXT, 255, ['nullable'  => false,], 'Location Name')
                  ->addColumn('longitude', Table::TYPE_FLOAT, null, [], 'Longitude')
                  ->addColumn('latitude', Table::TYPE_FLOAT, null, [], 'Latitude')
-                 ->addColumn('name', Table::TYPE_TEXT, null, [], 'Name')
                  ->addColumn('description', Table::TYPE_TEXT, null, [], 'Description');
         }
         $installer->getConnection()->createTable($table);
+
+        /**
+         * Create table 'cms_block_store'
+         */
+        $table = $installer->getConnection()->newTable(
+            $installer->getTable('fastgento_locator_store')
+        )->addColumn(
+            'location_id',
+            \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+            null,
+            ['nullable' => false, 'primary' => true],
+            'Location ID'
+        )->addColumn(
+            'store_id',
+            \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+            null,
+            ['unsigned' => true, 'nullable' => false, 'primary' => true],
+            'Store ID'
+        )->addIndex(
+            $installer->getIdxName('fastgento_locator_store', ['store_id']),
+            ['store_id']
+        )->addForeignKey(
+            $installer->getFkName('fastgento_locator_store', 'location_id', 'fastgento_locator', 'id'),
+            'location_id',
+            $installer->getTable('fastgento_locator'),
+            'location_id',
+            \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
+        )->addForeignKey(
+            $installer->getFkName('fastgento_locator_store', 'location_id', 'store', 'store_id'),
+            'store_id',
+            $installer->getTable('store'),
+            'store_id',
+            \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
+        )->setComment(
+            'Storelocation To Store Linkage Table'
+        );
+        $installer->getConnection()->createTable($table);
+
         $installer->endSetup();
     }
 }
